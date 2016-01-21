a43abb5f1cdad98b923111420aea713f7dd1b333
alphahacker <alphahacker@alphahacker-desktop>
2016-01-22 03:02:28 +0900

    [DeerInZooDivTwo][alphahacker][RV:N/A]

---
+++ Algorithm/deer.h
@@ -0,0 +1,20 @@
+/*
+ * deer.h
+ *
+ *  Created on: Jan 21, 2016
+ *      Author: ms-dev
+ */
+
+#ifndef _DEER_
+#define _DEER_
+
+#include "common.h"
+
+class DeerInZooDivTwo {
+
+public:
+	vector <int> getminmax (int N, int K);
+};
+
+#endif
+
