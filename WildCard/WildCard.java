dc650ef4d7b2e2c4e326b44daee2603e3380d5d2
KYEHEE <kkh0590@naver.com>
2016-02-24 19:49:26 +0900

    [WildCard][kkh0590][RV:N/A] 1. update!

--- WildCard/WildCard.java
+++ WildCard/WildCard.java
@@ -4,22 +4,16 @@ public class WildCard {
 	public String[] ex;
 	public String[] problem;
 	public int[] saveArr;
-	ArrayList<Integer> sepTokenQuestion; // save the location of *
-	ArrayList<String> sepTokenStar; // save the seperated token
+	ArrayList<String> character;
 
 	public static void main(String[] args) {
 		WildCard wildcard = new WildCard();
 		// wildcard.map = new HashMap<Integer, String>();
-		wildcard.sepTokenQuestion = new ArrayList<Integer>();
-		wildcard.sepTokenStar = new ArrayList<String>();
+		wildcard.character = new ArrayList<String>();
 
-		wildcard.split(wildcard.ex, wildcard.sepTokenQuestion, wildcard.sepTokenStar);
-		wildcard.compare(wildcard.ex, wildcard.problem, wildcard.sepTokenQuestion, wildcard.sepTokenStar,
-				wildcard.saveArr);
+		wildcard.split(wildcard.ex, wildcard.character);
+		wildcard.compare(wildcard.ex, wildcard.problem, wildcard.character, wildcard.saveArr);
 
-		// System.out.println(wildcard.sepTokenQuestion);
-
-		// System.out.println(wildcard.ex[0]);
 		for (int i = 0; i < wildcard.saveArr.length; i++) {
 			if (wildcard.saveArr[i] == 1) {
 				System.out.println(wildcard.problem[i]);
@@ -32,59 +26,51 @@ public class WildCard {
 		problem = new String[3];
 		saveArr = new int[3];
 
-		ex[0] = "h*p?p";
+		ex[0] = "h*p*p";
 		// ex[1] = "*p*";
 
-		problem[0] = "h222epop";
-		problem[1] = "hepop";
-		problem[2] = "nono";
+		problem[0] = "hlpap";
+		problem[1] = "helpop";
+		problem[2] = "aaaa";
 	}
 
-	public void split(String[] ex, ArrayList<Integer> sepTokenQuestion, ArrayList<String> sepTokenStar) {
+	public void split(String[] ex, ArrayList<String> character) {
 		for (int i = 0; i < ex.length; i++) {
 			StringTokenizer st = new StringTokenizer(ex[i], "*");
-			while (st.hasMoreTokens()) {
-				sepTokenStar.add(st.nextToken());
-			}
 
-			for (int j = 0; j < ex[i].length(); j++) {
-				if (ex[i].charAt(j) == '?') {
-					sepTokenQuestion.add(j);
-				}
+			while (st.hasMoreTokens()) {
+				character.add(st.nextToken());
 			}
 		}
-		System.out.println(sepTokenStar);
-		// System.out.println(sepTokenQuestion);
+
+		System.out.println(character);
 	}
 
-	public void compare(String[] ex, String[] problem, ArrayList<Integer> sepTokenQuestion,
-			ArrayList<String> sepTokenStar, int[] saveArr) {
+	public void compare(String[] ex, String[] problem, ArrayList<String> character, int[] saveArr) {
 		for (int i = 0; i < problem.length; i++) {
 			int result = 0;
 
-			for (int k = 0; k < sepTokenStar.size(); k++) {
-				int sentenceLoc = 0;
-				String sen = sepTokenStar.get(k);
+			int j;
+			int init = 0;
+			for (int k = 0; k < character.size(); k++) {
+				String sen = character.get(k);
 
-				for (int j = sentenceLoc; j < problem[i].length(); j++) {
+				for (j = init; j < problem[i].length();) {
+					System.out.println("k : " + k + "/ j : " + j);
 					if (j + sen.length() <= problem[i].length()) {
-						// System.out.println(problem[i].substring(j, j +
-						// sen.length()));
-						if (sen.equals(problem[i].substring(j, j + sen.length())) || (sen.contains("?")
-								&& problem[i].substring(j, j + sen.length()).length() == sen.length())) {
-							j = j + sen.length();
+						if (sen.equals(problem[i].substring(j, j + sen.length()))) {
+							init = j + sen.length();
 							result++;
 							break;
-						} else {
-
 						}
 					}
+					j++;
 				}
-				// System.out.println(result);
 			}
 
-			if (result == sepTokenStar.size()) {
+			if (result == character.size()) {
 				saveArr[i] = 1;
+
 			} else {
 				saveArr[i] = 0;
 			}
